6d57d457a9921a90c127c0f0abaafebd6977a3ca
hammadzaheen <hammadzaheen>
2023-01-31 18:29:55 +0500

    Need to resolve issues

--- MapsFencing/app/src/main/java/com/example/mapsfencing/SplashActivity.kt
+++ MapsFencing/app/src/main/java/com/example/mapsfencing/SplashActivity.kt
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.WindowManager
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.core.splashscreen.SplashScreen
@@ -12,6 +13,7 @@ import com.example.mapsfencing.admin.AdminMapsActivity
 import com.example.mapsfencing.authentication.LoginActivity
 import com.example.mapsfencing.preferences.PreferenceManager
 import com.example.mapsfencing.user.UserMapsActivity
+import com.google.firebase.auth.FirebaseAuth
 
 @SuppressLint("CustomSplashScreen")
 class SplashActivity : AppCompatActivity() {
@@ -31,10 +33,14 @@ class SplashActivity : AppCompatActivity() {
         preferenceManager = PreferenceManager(this@SplashActivity)
 
         if (preferenceManager.isLoggedIn()){
-            if (preferenceManager.getuser().username!!.contains("admin")){
+
+            Log.d("usernameSplash", "username: "+preferenceManager.getuser().username)
+
+            if (preferenceManager.getuser().username!!.contains("admin",true)){
                 startActivity(Intent(this@SplashActivity, AdminMapsActivity::class.java))
                 finish()
             }else{
+
                 startActivity(Intent(this@SplashActivity, UserMapsActivity::class.java))
                 finish()
             }
